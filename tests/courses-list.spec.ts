c2d62ca0d78037bdc85aae5935b396755f10179d
Bralin Temirlan <ludvig.born@gmail.com>
2024-02-04 15:49:04 +0600

    ci: add pull request check /test: add playwright and jest

---
+++ tests/courses-list.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('create delete course list', async ({ page }) => {
+  await page.goto('/');
+  await page.getByPlaceholder('название').click();
+  await page.getByPlaceholder('название').fill('test');
+  await page.getByPlaceholder('описание').click();
+  await page.getByPlaceholder('описание').click();
+  await page.getByPlaceholder('описание').fill('test');
+  await page.getByRole('button', { name: 'Добавить' }).click();
+  await expect(page.getByText('testtestУдалить')).toBeVisible();
+  await page.getByRole('button', { name: 'Удалить' }).click();
+  await expect(page.getByText('testtestУдалить')).not.toBeVisible();
+
+
+});
